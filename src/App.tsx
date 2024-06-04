0cc871995019c2e9a6aba00a0cb61b67d54848a0
Brady Lumsden <blumsden007@gmail.com>
2024-06-04 13:24:06 -0700

    update on all files finishing last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import "./App.css";
+import Container from "./Components/Container";
+import Footer from "./Components/Footer";
+import HashtagList from "./Components/HashtagList";
 
 export default function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Footer />
+      <Container />
+      <HashtagList />
+    </div>
+  );
 }
