62bfb2e122da2f3ded44f73a59a5a9b0d7373599
Brady Lumsden <blumsden007@gmail.com>
2024-08-16 00:41:23 -0700

    add useeffect to app.tsx and fetch data from bytegrad and console log it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
 import Container from "./Components/Container";
 import Footer from "./Components/Footer";
 import HashtagList from "./Components/HashtagList";
 
 export default function App() {
+  useEffect(() => {
+    const fetchInitialItems = async () => {
+      const res = await fetch(
+        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data);
+        return;
+      }
+      console.log(data);
+    };
+    fetchInitialItems();
+  }, []);
   return (
     <div className="app">
       <Footer />
